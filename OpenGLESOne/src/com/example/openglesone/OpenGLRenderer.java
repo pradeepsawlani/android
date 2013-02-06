9a808e9db60eb0f7adf2d194d8427d1f0a7c4717
Pradeep Sawlani <pradeep.sawlani@gmail.com>
2013-02-06 13:35:47 -0800

    Modify example one to demonstrate use of color array.
    Signed-off-by: Pradeep Sawlani <pradeep.sawlani@gmail.com>

--- OpenGLESOne/src/com/example/openglesone/OpenGLRenderer.java
+++ OpenGLESOne/src/com/example/openglesone/OpenGLRenderer.java
@@ -1,5 +1,6 @@
 package com.example.openglesone;
 
+import java.nio.Buffer;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
@@ -30,21 +31,35 @@ public class OpenGLRenderer implements Renderer {
 				1.0f, 0.0f, 0.0f,
 		};
 
+		float color[] = {
+				1.0f, 0.0f, 0.0f, 1.0f,
+				0.0f, 1.0f, 0.0f, 1.0f,
+				0.0f, 0.0f, 1.0f, 1.0f,
+		};
+
 		ByteBuffer vbb = ByteBuffer.allocateDirect(vertices.length * 4);
 		vbb.order(ByteOrder.nativeOrder());
 		FloatBuffer triangle = vbb.asFloatBuffer();
 		triangle.put(vertices);
 		triangle.position(0);
 
+		ByteBuffer cbb = ByteBuffer.allocateDirect(color.length * 4);
+		cbb.order(ByteOrder.nativeOrder());
+		FloatBuffer Colors = cbb.asFloatBuffer();
+		Colors.put(color);
+		Colors.position(0);
+
 		// TODO Auto-generated method stub
 		gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
 		gl.glRotatef(rotation, 0.0f, 0.0f, 1.0f);
 		gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+		gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
 		gl.glColor4f(1.0f, 0.0f, 0.0f, 1.0f);
 		gl.glVertexPointer(3, GL10.GL_FLOAT, 0, triangle);
+		gl.glColorPointer(4, GL10.GL_FLOAT, 0, Colors);
 		gl.glDrawArrays(GL10.GL_TRIANGLES, 0, 9);
 		gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-		rotation += 0.5;
+		//rotation += 0.5;
 
 
 	}
